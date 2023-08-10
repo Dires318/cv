c9d47ec3211bbf032b13e7dfb443336b3809abf2
Dires318 <getdera6591@gmail.com>
2023-08-10 08:29:39 +0300

    loading in html

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
+var dir = "./";
+// var dir "/cv/";
 
 $(function(){
-    $("#header").load("/cv/base/header.html"); 
+    $("#header").load(dir + "base/header.html"); 
+    $('head').append('<link rel="stylesheet" href="'+ dir + 'css/style.css" >');
     // $("#footer").load("footer.html"); 
   });
   
+
+  const render = (value) =>{
+    $("#body").load(dir + value)
+  }
