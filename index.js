2e7845d61a228944e181bdf3f7a551c09e730046
Dires318 <getdera6591@gmail.com>
2023-08-10 10:11:10 +0300

    add footer

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
-// var dir = "./";
-var dir = "/cv/";
+var dir = "./"; //local directory
+// var dir = "/cv/";  //github directory
 
 $(function(){
     $("#header").load(dir + "base/header.html"); 
     $('head').append('<link rel="stylesheet" href="'+ dir + 'css/style.css" >');
-    // $("#footer").load("footer.html"); 
+    $("#body").load(dir + "pages/home.html")
+    $("#footer").load(dir + "base/footer.html"); 
   });
   
 
