757698e3f29f84c7ced31b1da606231ac847de4b
dires <getdera6591@gmail.com>
2024-09-30 09:41:28 +0300

    update the routing

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-// var dir = "./"; //local directory
-var dir = "/cv/";  //github directory
+var dir = "/"; //local directory
+// var dir = "/cv/";  //github directory
 
 $(function(){
+    $("#head").load(dir + "base/head.html"); 
     $("#header").load(dir + "base/header.html"); 
     $('head').append('<link rel="stylesheet" href="'+ dir + 'css/style.css" >');
-    $("#body").load(dir + "pages/home.html")
+    // $("#body").load(dir + "pages/home.html")
     $("#footer").load(dir + "base/footer.html"); 
   });
   
 
-  const render = (value) =>{
-    $("#body").load(dir + value)
-  }
+  // const render = (value) =>{
+  //   $("#body").load(dir + value)
+  // }
